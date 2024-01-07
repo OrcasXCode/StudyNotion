385e68cc86d4d06dc56edead662bf9267f61a4c2
Om-Sureja <omsureja@gmail.com>
2024-01-07 23:55:24 +0530

    Completed Course route full with all other controller backend almost 90% complete rest remaining part is payment part and yes bugs are not solved yet

--- backend/controllers/Course.js
+++ backend/controllers/Course.js
@@ -1,49 +1,455 @@
-const {User}= require("../models/User")
-
-
-
+const Course = require("../models/Course");
+const User = require("../models/User");
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+const Section = require("../models/Section")
+const SubSection=require("../models/Subsection")
+const {convertSecondsToDuration} = require("../utils/secToDuration")
+const CourseProgress =require("../models/CourseProgress")
+const Category=require("../models/Category")
 
 async function createCourse(req,res){
-    try{
-        const userId=req.user.id;
+    try {
+        // Get user ID from request object
+        const userId = req.user.id
+    
+        // Get all required fields from request body
         let {
-            courseName,
-            courseDescription,
-            whatYouWillLearn,
-            price,
-            tag: _tag,
-            category,
-            status,
-            instructions: _instructions,
+          courseName,
+          courseDescription,
+          whatYouWillLearn,
+          price,
+          tag: _tag,
+          category,
+          status,
+          instructions: _instructions,
         } = req.body
-        const thumbnail=req.body.thumbnailImage;
-        const tag=JSON.parse(_tag);
-        const instructions=JSON.parse(_instructions)
+        // Get thumbnail image from request files
+        const thumbnail = req.files.thumbnailImage
+    
+        // Convert the tag and instructions from stringified Array to Array
+        const tag = JSON.parse(_tag)
+        const instructions = JSON.parse(_instructions)
+    
+        console.log("tag", tag)
+        console.log("instructions", instructions)
+    
+        // Check if any of the required fields are missing
+        if (
+          !courseName ||
+          !courseDescription ||
+          !whatYouWillLearn ||
+          !price ||
+          !tag.length ||
+          !thumbnail ||
+          !category ||
+          !instructions.length
+        ) {
+          return res.status(400).json({
+            success: false,
+            message: "All Fields are Mandatory",
+          })
+        }
+        if (!status || status === undefined) {
+          status = "Draft"
+        }
+        // Check if the user is an instructor
+        const instructorDetails = await User.findById(userId, {
+          accountType: "Instructor",
+        })
+    
+        if (!instructorDetails) {
+          return res.status(404).json({
+            success: false,
+            message: "Instructor Details Not Found",
+          })
+        }
+    
+        // Check if the tag given is valid
+        const categoryDetails = await Category.findById(category)
+        if (!categoryDetails) {
+          return res.status(404).json({
+            success: false,
+            message: "Category Details Not Found",
+          })
+        }
+        // Upload the Thumbnail to Cloudinary
+        const thumbnailImage = await uploadImageToCloudinary(
+          thumbnail,
+          process.env.FOLDER_NAME
+        )
+        console.log(thumbnailImage)
+        // Create a new course with the given details
+        const newCourse = await Course.create({
+          courseName,
+          courseDescription,
+          instructor: instructorDetails._id,
+          whatYouWillLearn: whatYouWillLearn,
+          price,
+          tag,
+          category: categoryDetails._id,
+          thumbnail: thumbnailImage.secure_url,
+          status: status,
+          instructions,
+        })
+    
+        // Add the new course to the User Schema of the Instructor
+        await User.findByIdAndUpdate(
+          {
+            _id: instructorDetails._id,
+          },
+          {
+            $push: {
+              courses: newCourse._id,
+            },
+          },
+          { new: true }
+        )
+        // Add the new course to the Categories
+        const categoryDetails2 = await Category.findByIdAndUpdate(
+          { _id: category },
+          {
+            $push: {
+              courses: newCourse._id,
+            },
+          },
+          { new: true }
+        )
+        console.log("HEREEEEEEEE", categoryDetails2)
+        // Return the new course and a success message
+        res.status(200).json({
+          success: true,
+          data: newCourse,
+          message: "Course Created Successfully",
+        })
+    }
+    catch(error){
+        // Handle any errors that occur during the creation of the course
+        console.error(error)
+        res.status(500).json({
+          success: false,
+          message: "Failed to create course",
+          error: error.message,
+        })
+    }
+}
 
-        if (!courseName || !courseDescription || !whatYouWillLearn || !price || !tag.length || !thumbnail || !category || !instructions.length){
-            return res.status(400).json({
-              success: false,
-              message: "All Fields are Mandatory",
+
+
+async function editCourse(req,res){
+    try{
+        const {courseId} = req.body;
+        const updates=req.body;
+
+        const course=await Course.findById(courseId)
+        if(!course){
+            console.log(error)
+            return res.status(404).json({
+                success:false,
+                msg:"Course not found"
             })
         }
 
-        if(!status || status == undefined){
-            status="Draft"
+        if(req.files){
+            console.log("thumbnail update")
+            const thumbnail=req.files.thumbnailImage
+            const thumbnailImage=await uploadImageToCloudinary(
+                thumbnail,
+                process.env.FOLDER_NAME
+            )
+            course.thumbnail=thumbnailImage.secure_url
         }
 
-        const instructorDetails=await User.findById(userId,{
-            accountType:"Instructor"
+        for(const key in updatess){
+            if(updates.hasOwnProperty(key)){
+                if(key==="tag" || key==="instructions"){
+                    course[key]=JSON.parse(updates[key])
+                }
+                else{
+                    course[key]=updates[key]
+                }
+            }
+        }
+
+        await course.save()
+        const updatedCourse=await Course.findOne({
+            _id:courseId
         })
+        .populate({
+            path: "instructor",
+            populate: {
+              path: "additionalDetails",
+            },
+        })
+        .populate("category")
+        .populate("ratingAndReviews")
+        .populate({
+            path: "courseContent",
+            populate: {
+              path: "subSection",
+            },
+        })
+        .exec()
 
-        if(!instructorDetails){
-            re
-        }
+        return res.status(200).json({
+            success:true,
+            msg:"Course updated successfully",
+            updatedCourse
+        })
     }
     catch(error){
-        console.log("Error in creating course: ", error);
+        console.log(error)
         return res.status(500).json({
             success:false,
-            msg:"Failed to create course",
+            msg:"internal server error"
         })
     }
+}
+
+
+
+async function getAllCourses(req,res){
+    try{
+        const allCourses=await Course.find(
+            {status:"Published"},
+            {
+                courseName: true,
+                price: true,
+                thumbnail: true,
+                instructor: true,
+                ratingAndReviews: true,
+                studentsEnrolled: true,
+            }
+        ).populate("instructor").exec()
+
+        return res.status(200).json({
+            success:true,
+            allCourses
+        })
+    }
+    catch(error){
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            msg:"Server error failed to get all courses"
+        })
+    }
+}
+
+
+async function getInstructorCourses(req,res){
+    try{
+        const instructorId=req.user.id
+
+        const instructorCourses=await Course.find({
+            instructor:instructorId
+        }).sort({createdAt:-1})
+
+        return res.status(200).json({
+            success:true,
+            instructorCourses
+        })
+    }
+    catch(error){
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            msg:"Server error failed to get instructor courses"
+        })
+    }
+}
+
+async function deleteCourse(req,res){
+    try{
+        const courseId=req.body
+
+        const course=await Course.findById(courseId)
+        if(!course){
+            return res.status(404).json({
+                success:false,
+                msg:"Course not found"
+            })
+        }
+
+        const studentsEnroled=course.studentsEnroled
+        for(const studentId of studentsEnroled){
+            await User.findByIdAndUpdate(studentId,
+                {
+                    $pull:{courses:courseId}
+                }
+            )
+        }
+
+        const courseSections=course.courseContent
+        for(const sectionId of courseSections){
+            const section=await Section.findById(sectionId)
+            if(section){
+                const subSections=section.subSection
+                for(const subSectionId of subSections){
+                    await SubSection.findByIdAndDelete(subSectionId)
+                }
+            }
+            await Section.findByIdAndDelete(sectionId)
+        }
+        await Course.findByIdAndDelete(courseId)
+
+        return res.status(200).json({
+            success:true,
+            msg:"Course deleted successfully"
+        })
+    }
+    catch(error){
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            msg:"Server error failed to delete the course"
+        })
+    }
+}
+
+
+async function getFullCourseDetails(req,res){
+    try {
+        const { courseId } = req.body
+        const userId = req.user.id
+        const courseDetails = await Course.findOne({
+          _id: courseId,
+        })
+        .populate({
+            path: "instructor",
+            populate: {
+              path: "additionalDetails",
+            },
+        })
+        .populate("category")
+        .populate("ratingAndReviews")
+        .populate({
+            path: "courseContent",
+            populate: {
+              path: "subSection",
+            },
+        })
+        .exec()
+    
+        let courseProgressCount = await CourseProgress.findOne({
+          courseID: courseId,
+          userId: userId,
+        })
+    
+        console.log("courseProgressCount : ", courseProgressCount)
+    
+        if (!courseDetails) {
+          return res.status(400).json({
+            success: false,
+            message: `Could not find course with id: ${courseId}`,
+          })
+        }
+    
+        // if (courseDetails.status === "Draft") {
+        //   return res.status(403).json({
+        //     success: false,
+        //     message: `Accessing a draft course is forbidden`,
+        //   });
+        // }
+    
+        let totalDurationInSeconds = 0
+        courseDetails.courseContent.forEach((content) => {
+          content.subSection.forEach((subSection) => {
+            const timeDurationInSeconds = parseInt(subSection.timeDuration)
+            totalDurationInSeconds += timeDurationInSeconds
+          })
+        })
+    
+        const totalDuration = convertSecondsToDuration(totalDurationInSeconds)
+    
+        return res.status(200).json({
+          success: true,
+          data: {
+            courseDetails,
+            totalDuration,
+            completedVideos: courseProgressCount?.completedVideos
+              ? courseProgressCount?.completedVideos
+              : [],
+          },
+        })    
+    } 
+    catch(error){
+        return res.status(500).json({
+          success: false,
+          message: error.message,
+        })
+    }
+}
+
+
+async function getCourseDetails(req,res){
+    try {
+        const { courseId } = req.body
+        const courseDetails = await Course.findOne({
+          _id: courseId,
+        })
+        .populate({
+            path: "instructor",
+            populate: {
+              path: "additionalDetails",
+            },
+        })
+        .populate("category")
+        .populate("ratingAndReviews")
+        .populate({
+            path: "courseContent",
+            populate: {
+              path: "subSection",
+              select: "-videoUrl",
+            },
+        })
+        .exec()
+    
+        if (!courseDetails) {
+          return res.status(400).json({
+            success: false,
+            message: `Could not find course with id: ${courseId}`,
+          })
+        }
+    
+        // if (courseDetails.status === "Draft") {
+        //   return res.status(403).json({
+        //     success: false,
+        //     message: `Accessing a draft course is forbidden`,
+        //   });
+        // }
+    
+        let totalDurationInSeconds = 0
+        courseDetails.courseContent.forEach((content) => {
+          content.subSection.forEach((subSection) => {
+            const timeDurationInSeconds = parseInt(subSection.timeDuration)
+            totalDurationInSeconds += timeDurationInSeconds
+          })
+        })
+    
+        const totalDuration = convertSecondsToDuration(totalDurationInSeconds)
+    
+        return res.status(200).json({
+          success: true,
+          data: {
+            courseDetails,
+            totalDuration,
+          },
+        })
+    } 
+    catch(error){
+        return res.status(500).json({
+          success: false,
+          message: error.message,
+        })
+    }
+}
+
+module.exports={
+    createCourse,
+    editCourse,
+    getAllCourses,
+    getInstructorCourses,
+    deleteCourse,
+    getFullCourseDetails,
+    getCourseDetails
 }
--- backend/routes/Course.js
+++ backend/routes/Course.js
@@ -1,6 +1,37 @@
-const express=require("express")
+const express=require("express");
+const { isInstructor, auth, isAdmin, isStudent } = require("../middleware/auth");
+const { createCourse, editCourse, getInstructorCourses, getAllCourses, getCourseDetails, deleteCourse, getFullCourseDetails } = require("../controllers/Course");
+const { createSection, updateSection, deleteSection } = require("../controllers/Section");
+const { updatedSubSection, deleteSubSection, createSubSection } = require("../controllers/Subsection");
+const { getAllRatingReview, getAverageRating, createRating } = require("../controllers/RatingandReview");
+const { updateCourseProgress } = require("../controllers/courseProgress");
+const { createCategory, showAllCategories, categoryPageDetails } = require("../controllers/Category");
 const router=express.Router();
 
+
+
+router.post("/createCourse",auth,isInstructor,createCourse)
+router.post("/editCourse",auth,isInstructor,editCourse)
+router.post("/addSection",auth,isInstructor,createSection)
+router.post("/updateSection",auth,isInstructor,updateSection)
+router.post("/deleteSection",auth,isInstructor,deleteSection)
+router.post("/updateSubSection",auth,isInstructor,updatedSubSection)
+router.post("/deleteSubSection",auth,isInstructor,deleteSubSection)
+router.post("/addSubSection",auth,isInstructor,createSubSection)
+router.post("/getInstructorCourses",auth,isInstructor,getInstructorCourses)
+router.post("/getAllCourses",getAllCourses)
+router.post("/getCourseDetails",getCourseDetails)
+router.post("/getFullCourseDetails", auth, getFullCourseDetails)
+router.post("/updateCourseProgress", auth, isStudent, updateCourseProgress)
+router.delete("/deleteCourse", deleteCourse)
+router.post("/createCategory", auth, isAdmin, createCategory)
+router.get("/showAllCategories", showAllCategories)
+router.post("/getCategoryPageDetails", categoryPageDetails)
+router.post("/createRating", auth, isStudent, createRating)
+router.get("/getAverageRating", getAverageRating)
+router.get("/getReviews", getAllRatingReview)
+
+module.exports = router
 
 
 
