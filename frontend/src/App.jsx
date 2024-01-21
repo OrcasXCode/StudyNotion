6f74b776907ca2d02cc0163e10e7e56b9cbf7425
Om-Sureja <omsureja@gmail.com>
2024-01-21 23:32:35 +0530

    added homepage and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from '../components/Common/Navbar'
+import Home from '../pages/Home'
+import { Route, Routes } from 'react-router-dom'
+
+
+
 
 function App() {
 
   return (
-    <>
-      <p>Hy</p>
-    </>
+     <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </div>
   )
 }
 
